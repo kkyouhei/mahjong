3caf1baeb3a77870d5cbd7882f0eb0ceb41421f8
Kyohei Karino <hope637@gmail.com>
2012-09-17 22:40:13 +0900

    テストコード追加とメソッド3行以内、クラス50行以内の縛りプレイを追加

--- php/Tile.php
+++ php/Tile.php
@@ -1,69 +1,25 @@
 <?php
 
-class Tile{
+class Tile
+{
+	// 牌の総数
+	const TILES_COUNT = 136;
 	
-	private $tile;
-	
-	function __construct(){
-//		$this->makeMountain();
+	// マンピンソー字の順にIDとSRC属性の中身を保持する配列
+	var $tile = array();
+
+	function __construct()
+	{
+		$this->createTile();
 	}
 
-	private function tileImgAndIdPush($tile, $img, $id){
-		$tile[$i]['img'] = $img;
-		$tile[$i]['id'] = $id ;
-		return $tile;
-	}
-
-	private function manzuPair($tile, $pair){
-		// $tile push manzu 
-		for($i=1 ; $i<10 ; $i++){
-			$this->tile = tileImgAndIdPush($tile, 'ma' . $pair . $i, 'ma' . $id);
+	/**
+	 * 	配列の初期化 
+	 */
+	private function createTile()
+	{
+		for($i=0 ; $i<self::TILES_COUNT ; $i++){
+			$this->tile[$i] = array('src'=>'', 'id'=>'');
 		}
 	}
-
-	private function setThePair(){
-		
-	}
-
-/*
-	private function pinzuSet($tile){
-		// $tile push pinzu 
-		for($i=1 ; $i<10 ; $i++){
-			$this->tile = tileImgAndIdPush($tile, 'pi' . $i, 'pi' . $id);
-		}
-	}
-	
-	private function sozuSet($tile){
-		// $tile push sozu 
-		for($i=1 ; $i<10 ; $i++){
-			$this->tile = tileImgAndIdPush($tile, 'so' . $i, 'so' . $id);
-		}
-	}
-	
-	private function zihaiSet($tile){
-		// $tile push zihai 
-		for($i=1 ; $i<8 ; $i++){
-			$this->tile = tileImgAndIdPush($tile, 'zi' . $i, 'zi' . $id);
-		}	}
-
-	private function makeMountain(){
-
-		$i = 0;
-		// 要素内でidを一意にするために付番
-		$tileSet = 1;
-		while($i<135){
-			$tileSet++;
-		}
-		$this->setTile($tile);
-
-	}
-
-	public function setTile($tile){
-		$this->tile = $tile;
-	}
-
-	public function getTile(){
-		return $this->tile;
-	}
-*/
 }
--- php/TileTest.php
+++ php/TileTest.php
@@ -5,42 +5,9 @@ require_once 'Tile.php';
 
 class TileTest extends PHPUnit_Framework_TestCase
 {
-	/**
-	 *  
-	 */
-	public function test配列？()
+	public function testCreateTile_Tile配列にTILES_COUNT分要素あるか？()
 	{
 		$tile = new Tile();
-		$this->assertTrue(is_array($tile->getTile()));
+		$this->assertEquals(Tile::TILES_COUNT, count($tile->tile));
 	}
-
-	public function test牌の総数は136()
-	{
-		$tile = new Tile();	
-		$this->assertEquals(
-			136,
-			count($tile->getTile())
-		);
-	}
-
-	/**
-	 * tileImgIdSet
-	 */
-	public function test牌のurlとidはセットされてる？(){
-		$tileClass = new Tile();
-		$tile = $tileClass->tileImgAndIdSet(array(), 'test/test.png', 'test');
-		$this->assertEquals('test/test.png', array_pop($tile['url']));
-		$this->assertEquals('test', array_pop($tile['id']));
-		$this->assertEquals(1, count($tile));
-	}
-
-
-	/**
-	 * manzuOnePiece
-	 */
-	/*
-	public function test配列？(){
-		$tile = new Tile();
-	}
-	 */
 }
