db5c6e2b976790b73e305b40a2acd452fa5dc144
Kyohei Karino <hope637@gmail.com>
2012-10-09 22:50:34 +0900

    帰宅コミット

--- js/mahjong.js
+++ js/mahjong.js
@@ -1,63 +1,30 @@
-var tile = function(src, id, width, height, $drawtarget){
-	var $element = $drawtarget.append('<div id=' + id  +'></div>');
-	$element.css({
-		position: 'absolute',
-		width: width,
-		height: height//,
-//		backgroundimage: 'url(' + src + ')'
-	});
-//	var elemstyle = $element[0].style;
-
-	var that = {
-		draw: function(x, y, src, id, $drawtarget){
-			$drawtarget.css({
-				position: 'absolute',
-				width: x,
-				height: y,
-				backgroundImage: 'url(' + src + ')',
-			left : 100 + 'px',
-			top : 100 + 'px'
-			});
-			//elemStyle.left = x + 'px';
-			//elemStyle.top = y + 'px';
-		},
-
-		show: function(){
-//			elemStyle.display = 'block';
-		},
-
-		hide: function(){
-//			elemStyle.display = 'none';
-		},
-
-		destroy: function(){
-//			$element.remove();
-		}
-	}; 
-	return that;
-};
-
 function tileCreater(tile){
 	var src;
 	var id;
-	var topLine = 100;
-	var leftLine = 100;
+	var topLine = 0;
+	var leftLine = 0;
 	var $drawTarget =  $('#myArea');
-	
+	var j = 0;	
 	for(var i=0 ; i<tile.length ; i++){
+		j++;
 		src = './image/' + tile[i]['src'] + '.gif';
 		id = tile[i]['id']; 
 
 		var $element = $drawTarget.append('<div id=' + id  +'></div>').find("#" + id);
 		$element.css({
+			position: 'absolute',
 			width: 80,
-			height: 120,
+			height: 112,
 			top: topLine,
 			left: leftLine,
 			backgroundImage: 'url(' + src + ')'
 		});
-		topLine = topLine + 100;
-		leftLine = topLine + 100;
+		leftLine = leftLine + 80;
+		if(i==11 || i==22){
+			leftLine = 0;
+			topLine = topLine +150;  
+		}
+
 	}
 }
 
